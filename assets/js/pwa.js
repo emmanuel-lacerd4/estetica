007aa2034204f940e03675af6230fe428f185292
EMMANUEL LACERDA NOGUEIRA <emmanuel.lnogueira@senacsp.edu.br>
2025-04-11 20:16:10 -0300

    estetica

--- assets/js/pwa.js
+++ assets/js/pwa.js
@@ -3,19 +3,19 @@ if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
         navigator.serviceWorker.register('/service_worker.js')
             .then((registration) => {
-                console.log('ServiceWorker registrado com sucesso:', registration.scope);
+                console.log('ServiceWorker registrado com sucesso:', registration.scope)
             })
             .catch((error) => {
-                console.log('Falha no registro do ServiceWorker:', error);
+                console.log('Falha no registro do ServiceWorker:', error)
             });
     });
 }
 
 // Trigger para instalação do PWA
-let deferredPrompt;
+let deferredPrompt
 window.addEventListener('beforeinstallprompt', (e) => {
-    e.preventDefault();
-    deferredPrompt = e;
+    e.preventDefault()
+    deferredPrompt = e
     // Mostra um botão "Instalar" personalizado aqui (opcional)
-    console.log('PWA pronto para instalação!');
-});
+    console.log('PWA pronto para instalação!')
+})
